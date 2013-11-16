9ece979899fdf581479e49152b658ca1eae30f70
Andrew Dehn <adehn3@gatech.edu>
2013-11-16 15:39:41 -0500

    Added CAS authentication

--- android/TechBook/gen/edu/gatech/techbook/R.java
+++ android/TechBook/gen/edu/gatech/techbook/R.java
@@ -22,10 +22,14 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int tblogo=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
-        public static final int menu_settings=0x7f080002;
+        public static final int action_settings=0x7f080006;
+        public static final int initLoginButton=0x7f080003;
+        public static final int initTechbookLogo=0x7f080002;
+        public static final int loggedinTechbookLogo=0x7f080004;
+        public static final int menu_settings=0x7f080005;
         public static final int mycoursesTextView=0x7f080001;
         public static final int recentForumsTextView=0x7f080000;
     }
@@ -34,21 +38,26 @@ public final class R {
         public static final int activity_forum=0x7f030001;
         public static final int activity_home=0x7f030002;
         public static final int activity_init=0x7f030003;
+        public static final int activity_logged_in=0x7f030004;
     }
     public static final class menu {
         public static final int activity_init=0x7f070000;
         public static final int course=0x7f070001;
         public static final int forum=0x7f070002;
         public static final int home=0x7f070003;
+        public static final int logged_in=0x7f070004;
     }
     public static final class string {
         public static final int action_settings=0x7f050004;
         public static final int app_name=0x7f050000;
+        public static final int button_login=0x7f050007;
         public static final int hello_world=0x7f050001;
         public static final int menu_settings=0x7f050002;
+        public static final int techbook_logo_description=0x7f050009;
         public static final int title_activity_course=0x7f050005;
         public static final int title_activity_forum=0x7f050006;
         public static final int title_activity_home=0x7f050003;
+        public static final int title_activity_logged_in=0x7f050008;
     }
     public static final class style {
         /** 
--- android/TechBook/src/edu/gatech/techbook/InitActivity.java
+++ android/TechBook/src/edu/gatech/techbook/InitActivity.java
@@ -1,15 +1,26 @@
 package edu.gatech.techbook;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class InitActivity extends Activity {
+public class InitActivity extends Activity  implements OnClickListener{
 
+	private Button loginButton;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_init);
+        
+        loginButton = (Button) findViewById(R.id.initLoginButton);
+        loginButton.setOnClickListener(this);
     }
 
     @Override
@@ -18,5 +29,18 @@ public class InitActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_init, menu);
         return true;
     }
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		Log.d("InitActivity", "A button was pressed");
+		if(v == loginButton) {
+			Log.d("InitActivity", "Login button was pressed");
+			Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://dev.m.gatech.edu/login?url=techbook://loggedin&sessionTransfer=window"));
+			startActivity(myIntent);
+		} else {
+			Log.d("InitActivity", "Some other button was pressed");
+		}
+	}
     
 }
